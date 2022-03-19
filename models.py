0e7fee8c0ede5130fe8cc4fbb705d7bf24c8635a
nh124 <61254430+nh124@users.noreply.github.com>
2022-03-19 14:24:05 -0400

    Updated phone number

--- models.py
+++ models.py
@@ -6,7 +6,7 @@ class Users(db.Model,UserMixin):
     Fname = db.Column(db.String(50), nullable=False)
     Lname = db.Column(db.String(50), nullable=False)
     Email = db.Column(db.String(50), nullable=False)
-    Pnumber = db.Column(db.Integer , nullable=False)
+    Pnumber = db.Column(db.String(50) , nullable=False)
     password = db.Column(db.String(200), nullable=False)
     username = db.Column(db.String(50), nullable=False, unique=True)
     status = db.Column(db.Integer, nullable=False)
